ac5a89e7ea497de476b1d596f1620f67cb326a01
John Dyer <johntdyer@gmail.com>
2012-10-15 10:26:49 -0400

    Fixed default brandId in rest-context.xml.  This is required in 11.7 and will be fixed in 11.8 [OPS-164]

--- provisioning.mysql.sh
+++ provisioning.mysql.sh
@@ -21,4 +21,8 @@ sed -i -e"s/com.tropo.provisioning.jms.username=.*/com.tropo.provisioning.jms.us
 sed -i -e"s/com.tropo.provisioning.jms.password=.*/com.tropo.provisioning.jms.password=admin/" /opt/voxeo/prism/apps/rest/WEB-INF/tropo.properties
 sed -i -e"s/com.tropo.provisioning.jms.notifications.topic=.*/com.tropo.provisioning.jms.notifications.topic=VirtualTopic.Provisioning/" /opt/voxeo/prism/apps/rest/WEB-INF/tropo.properties
 
+## This should be fixed in REST 11.8 but for now we have to fix it here
+sed -i -e"s/com.tropo.provisioning.default.brand:null/com.tropo.provisioning.default.brand:9/" /opt/voxeo/prism/apps/rest/WEB-INF/rest-context.xml
+
 cd $HOME/tiab_manual_installation
+
