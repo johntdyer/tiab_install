4b4de5458bed3936a50508bbfa7f8fc2cea83104
John Dyer <johntdyer@gmail.com>
2012-10-15 10:18:18 -0400

    Fixed semicolon in post install [OPS-164]

--- post.install.sh
+++ post.install.sh
@@ -2,10 +2,10 @@
 set -e
 set -u
 
-/opt/voxeo/prism/bin/prism start 
+/opt/voxeo/prism/bin/prism start
 
-# Set the hostname for the provisioning API, you can add multiple hostnames if you want. 
-if [ -f '/opt/voxeo/prism/shared/lib/h2-1.3.164.jar' ] then;
+# Set the hostname for the provisioning API, you can add multiple hostnames if you want.
+if [ -f '/opt/voxeo/prism/shared/lib/h2-1.3.164.jar' ]; then
     /opt/voxeo/prism/jre/bin/java -cp /opt/voxeo/prism/shared/lib/h2-1.3.164.jar org.h2.tools.Shell -user sa -password "" -url jdbc:h2:tcp://localhost:9092/provisioning -sql "insert into AuthorizedRemoteAddress(id,pattern,accountid) values(5, 'your.server.homename.com',1);"
 fi
 
